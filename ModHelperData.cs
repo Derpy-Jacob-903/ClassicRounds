8992daf823d02c97d88cdf188ba7f619d5a1a666
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2024-10-12 20:54:58 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,11 +2,11 @@ namespace ClassicRounds;
 #pragma warning disable 1591
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "43.0";
+    public const string WorksOnVersion = "45.0";
     public const string Version = "0.2.0";
     public const string Name = "ClassicRounds";
 
-    public const string Description = "";
+    public const string Description = "Adds the round sets from BTD3 and BTD4, as well as new game modes that use these round sets, with added tower and upgrade restrictions.";
 
     public const string RepoOwner = "Derpy-Jacob-903"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "ClassicRounds"; // TODO add your repo name here, also in the download url in README.md
